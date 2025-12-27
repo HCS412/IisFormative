46bd3e72244f4237cb56869dac1bdf32debfab04
Brandon Brooks <bbrooksvc@gmail.com>
2025-12-27 11:39:56 -0500

    Initial Commit

---
+++ FormativeiOS/FormativeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormativeiOSApp.swift
+//  FormativeiOS
+//
+//  Created by Brandon Brooks on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FormativeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormativeiOSTests/FormativeiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FormativeiOSTests.swift
+//  FormativeiOSTests
+//
+//  Created by Brandon Brooks on 12/27/25.
+//
+
+import Testing
+@testable import FormativeiOS
+
+struct FormativeiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
